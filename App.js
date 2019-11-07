600c90a0ffb32341d19b5d06712b59cfb2947b77
Aman Mittal <amandeepmittal@live.com>
2019-11-07 17:56:01 +0530

    add eva-icons pack

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { mapping, light as lightTheme } from '@eva-design/eva'
-import { ApplicationProvider } from 'react-native-ui-kitten'
+import { ApplicationProvider, IconRegistry, Icon } from 'react-native-ui-kitten'
+import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import { View, Text } from 'react-native'
 
 const MockComponent = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>React Native Instagram Clone</Text>
+    <Icon name='wifi-outline' width={50} height={50} />
   </View>
 )
 
 const App = () => (
-  <ApplicationProvider mapping={mapping} theme={lightTheme}>
-    <MockComponent />
-  </ApplicationProvider>
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <MockComponent />
+    </ApplicationProvider>
+  </>
 )
 
 export default App
