eae553a1e18c8e9f5a345e74ef25cade5a9bfad2
Aman Mittal <amandeepmittal@live.com>
2019-11-08 10:21:11 +0530

    screen: Login

---
+++ src/screens/Login.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { Text, Button, Input, Icon } from 'react-native-ui-kitten'
+
+class Login extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: '#fff', paddingHorizontal: 30 }}>
+        <View style={{ alignItems: 'center', marginTop: 60 }}>
+          <Icon name='log-in-outline' width={200} height={200} fill='#333' />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Input
+            style={{ marginTop: 10, fontSize: 16 }}
+            label='Email'
+            autoCapitalize='none'
+            autoCompleteType='email'
+            keyboardType='email-address'
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Input
+            style={{ marginTop: 10, fontSize: 16 }}
+            label='Password'
+            secureTextEntry={true}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Button status='warning' onPress={() => alert('Login')}>
+            Login
+          </Button>
+        </View>
+        <View
+          style={{
+            marginTop: 10,
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'row'
+          }}>
+          <Text style={{ textAlign: 'center', fontSize: 14, color: '#abb4bd' }}>
+            Don't have an account?{' '}
+          </Text>
+          <Button
+            status='info'
+            appearance='ghost'
+            style={{
+              paddingHorizontal: 1
+            }}>
+            Sign up
+          </Button>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default Login
