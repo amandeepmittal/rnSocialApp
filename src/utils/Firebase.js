55250ef54d08c0dfae85389da9d7494aada4579d
Aman Mittal <amandeepmittal@live.com>
2019-11-08 12:34:00 +0530

    add upload and fetch post methods

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -22,36 +22,36 @@ const Firebase = {
       .collection('users')
       .doc(`${userData.uid}`)
       .set(userData)
+  },
+  uploadPost: post => {
+    const id = uuid.v4()
+    const uploadData = {
+      id: id,
+      postPhoto: post.photo,
+      postTitle: post.title,
+      postDescription: post.description,
+      likes: []
+    }
+    return firebase
+      .firestore()
+      .collection('posts')
+      .doc(id)
+      .set(uploadData)
+  },
+  getPosts: () => {
+    return firebase
+      .firestore()
+      .collection('posts')
+      .get()
+      .then(function(querySnapshot) {
+        let posts = querySnapshot.docs.map(doc => doc.data())
+        // console.log(posts)
+        return posts
+      })
+      .catch(function(error) {
+        console.log('Error getting documents: ', error)
+      })
   }
-  // uploadPost: post => {
-  //   const id = uuid.v4()
-  //   const uploadData = {
-  //     id: id,
-  //     postPhoto: post.photo,
-  //     postTitle: post.title,
-  //     postDescription: post.description,
-  //     likes: []
-  //   }
-  //   return firebase
-  //     .firestore()
-  //     .collection('posts')
-  //     .doc(id)
-  //     .set(uploadData)
-  // },
-  // getPosts: () => {
-  //   return firebase
-  //     .firestore()
-  //     .collection('posts')
-  //     .get()
-  //     .then(function (querySnapshot) {
-  //       let posts = querySnapshot.docs.map(doc => doc.data())
-  //       // console.log(posts)
-  //       return posts
-  //     })
-  //     .catch(function (error) {
-  //       console.log('Error getting documents: ', error)
-  //     })
-  // }
 }
 
 export default Firebase
