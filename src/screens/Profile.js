194d56dff777fd14dbe13cb47c9cfe4438d1f45e
Aman Mittal <amandeepmittal@live.com>
2019-11-17 01:20:46 +0530

    Creating a User Profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { Button, Icon, Text } from 'react-native-ui-kitten'
+import { Text, Button, withStyles, Avatar } from 'react-native-ui-kitten'
 import { withFirebaseHOC } from '../utils'
+import Gallery from '../components/Gallery'
 
-class Profile extends Component {
+class _Profile extends Component {
+  state = {
+    images: []
+  }
+
+  componentDidMount() {
+    this.fetchPosts()
+  }
+
+  fetchPosts = async () => {
+    try {
+      const posts = await this.props.firebase.getUserPosts()
+      let images = posts.map(item => {
+        return item.postPhoto
+      })
+
+      this.setState({ images })
+      console.log(this.state.images)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   handleSignout = async () => {
     try {
       await this.props.firebase.signOut()
@@ -14,26 +37,113 @@ class Profile extends Component {
   }
 
   render() {
+    const { images } = this.state
+    const { themedStyle } = this.props
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}>
-        <Text>Profile</Text>
-        <Button
-          onPress={this.handleSignout}
-          titleStyle={{
-            color: '#F57C00'
-          }}
-          type='clear'>
-          Signout
-        </Button>
+      <View style={themedStyle.root}>
+        <View style={[themedStyle.header, themedStyle.bordered]}>
+          <Avatar
+            source={{
+              uri:
+                'https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+            }}
+            size='giant'
+            style={{ width: 100, height: 100 }}
+          />
+          <Text category='h6' style={themedStyle.text}>
+            Test User
+          </Text>
+        </View>
+        <View style={[themedStyle.userInfo, themedStyle.bordered]}>
+          <View style={themedStyle.section}>
+            <Text category='s1' style={themedStyle.space}>
+              {images.length}
+            </Text>
+            <Text appearance='hint' category='s2'>
+              Posts
+            </Text>
+          </View>
+          <View style={themedStyle.section}>
+            <Text category='s1' style={themedStyle.space}>
+              0
+            </Text>
+            <Text appearance='hint' category='s2'>
+              Followers
+            </Text>
+          </View>
+          <View style={themedStyle.section}>
+            <Text category='s1' style={themedStyle.space}>
+              0
+            </Text>
+            <Text appearance='hint' category='s2'>
+              Following
+            </Text>
+          </View>
+        </View>
+        <View style={themedStyle.buttons}>
+          <Button
+            style={themedStyle.button}
+            appearance='ghost'
+            status='danger'
+            onPress={this.handleSignout}>
+            LOGOUT
+          </Button>
+          <View style={themedStyle.separator} />
+          <Button style={themedStyle.button} appearance='ghost' status='danger'>
+            MESSAGE
+          </Button>
+        </View>
+        <Gallery items={images} />
       </View>
     )
   }
 }
 
-export default withFirebaseHOC(Profile)
+export default Profile = withFirebaseHOC(
+  withStyles(_Profile, theme => ({
+    root: {
+      backgroundColor: theme['color-basic-100'],
+      marginTop: 60
+    },
+    header: {
+      alignItems: 'center',
+      paddingTop: 25,
+      paddingBottom: 17
+    },
+    userInfo: {
+      flexDirection: 'row',
+      paddingVertical: 18
+    },
+    bordered: {
+      borderBottomWidth: 1,
+      borderColor: theme['color-basic-400']
+    },
+    section: {
+      flex: 1,
+      alignItems: 'center'
+    },
+    space: {
+      marginBottom: 3,
+      color: theme['color-basic-1000']
+    },
+    separator: {
+      backgroundColor: theme['color-basic-400'],
+      alignSelf: 'center',
+      flexDirection: 'row',
+      flex: 0,
+      width: 1,
+      height: 42
+    },
+    buttons: {
+      flexDirection: 'row',
+      paddingVertical: 8
+    },
+    button: {
+      flex: 1,
+      alignSelf: 'center'
+    },
+    text: {
+      color: theme['color-basic-1000']
+    }
+  }))
+)
